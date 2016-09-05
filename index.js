c195084ca27655785c936825822651256d52d544
Boris Shabanov <boris-shabanov@yandex.ru>
2016-09-05 11:53:19 +0300

    set png transformer

--- index.js
+++ index.js
@@ -135,7 +135,7 @@ module.exports = {
                         '-jar',
                         'plantuml.jar',
                         '-nbthread auto',
-                        //'-tsvg',
+                        '-tpng',
                         umlPath,
                         '-o',
                         '.'
